e9868e5cd9e951d330ecd7fa5c7dfd2327ae1e74
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-01-18 20:32:26 +0530

    Add files via upload

---
+++ Day1/index.js
@@ -0,0 +1,23 @@
+const cursor= document.getElementById("cursor")
+window.addEventListener("mousemove",(e)=>{
+    cursor.style.left=e.pageX +"px";
+    cursor.style.top= e.pageY +"px";
+    cursor.setAttribute("data-from-top",(cursor.offsetTop-scrollY))
+
+});
+window.addEventListener("scroll",()=>{
+    const fromtop=parseInt(cursor.getAttribute('data-from-top'));
+
+    cursor.style.top= scrollY+fromtop+"px";
+});
+window.addEventListener("click",()=>{
+    if(cursor.classList.contains('click')){
+        cursor.classList.remove("click");
+        void cursor.offsetWidth;
+        cursor.classList.add("click");
+    }else{
+
+        cursor.classList.add("click");
+    }
+    
+});
