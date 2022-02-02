b6c63b66ee89bc6d05587d683b7c2731f53fe3f3
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-02 09:29:28 +0530

    Clock

--- day5/index.js
+++ day5/index.js
@@ -1,6 +1,7 @@
 const minEl = document.querySelector(".min");
 const secEl = document.querySelector(".sec");
 const hourEl = document.querySelector(".hour");
+const showdate = document.getElementById("inputDate");
 
 setInterval(() => {
   const date = new Date();
@@ -10,6 +11,15 @@ setInterval(() => {
   secEl.style.transform = `rotate(${secDeg}deg)`;
   minEl.style.transform = `rotate(${minDeg}deg)`;
   hourEl.style.transform = `rotate(${hourDeg}deg)`;
+  let hours= date.getHours()<10 ? `0${date.getHours()}`:date.getHours();
+  let min= date.getMinutes()<10 ? `0${date.getMinutes()}`:date.getMinutes();
+
+  let sec=date.getSeconds()<10?`)${date.getSeconds()}`:date.getSeconds();
+  showdate.value=`${hours}:${min}:${sec}`;
+ 
+  
+  
+
   // console.log(date.getSeconds())
   // console.log(date.getMinutes())
 }, 1000);
